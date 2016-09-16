db28a5709c11491e1ea6b800b1aa4429fb371b20
LitenApe <sonvo95@outlook.com>
2016-09-16 13:59:13 +0200

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,4 +1,7 @@
-import requests, json,datetime
+import requests,datetime
+import simplejson as json # pip install simplejson
+# if you dont want to use simple json, just uncomment the line under and remove the line over
+# import json
 
 # get current day
 now = datetime.datetime.now()
@@ -8,9 +11,10 @@ day = "&day=" + str(now.day)
 
 # URL for get requests
 URL = "https://holidayapi.com/v1/holidays?key=3e2a217e-eb23-492c-9de4-4dd8b9b78d0f&country=NO" + year + month + day
+
 # send get request and retrieve json
 r = requests.get(URL)
-js = json.loads(r.text)
+js = json.loads(r.text) # Why you so slow man....
 
 if js["status"] == 200: # Green lightm nothing went wrong
     if len(js["holidays"]) == 1: # It is a holiday
